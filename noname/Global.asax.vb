85cb423e59a70c87aaa875527be7a251008f58af
KiriMihi <kirimihi@rambler.ru>
2014-03-06 21:22:25 +0400

    Hello

---
+++ noname/Global.asax.vb
@@ -0,0 +1,15 @@
+﻿' Примечание: Инструкции по включению классического режима IIS6 или IIS7 
+' см. по ссылке http://go.microsoft.com/?LinkId=9394802
+Imports System.Web.Http
+
+Public Class MvcApplication
+    Inherits System.Web.HttpApplication
+
+    Sub Application_Start()
+        AreaRegistration.RegisterAllAreas()
+
+        WebApiConfig.Register(GlobalConfiguration.Configuration)
+        FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters)
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+    End Sub
+End Class
---
+++ noname/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' общие сведения об этой сборке.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("noname")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("noname")> 
+<Assembly: AssemblyCopyright("© , 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+' Следующий GUID служит для ID-идентификации библиотеки типов, если этот проект видим для COM
+<Assembly: Guid("cf58ef79-52a8-4501-9039-5bb3d2ff20f3")> 
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер сборки
+'      Редакция
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
