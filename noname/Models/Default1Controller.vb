9837bd5289ed1bae72389b4f1a02796dd46c3670
KiriMihi <kirimihi@rambler.ru>
2014-03-06 22:02:57 +0400

    2 change

---
+++ noname/Models/Default1Controller.vb
@@ -0,0 +1,13 @@
+﻿Public Class Default1Controller
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /Default1
+
+    Function Index() As ActionResult
+        Return View()рпасчв маприодтлэж
+        жЭЮДБлропасчв мспродлжэ
+        эюоирмпсиьобтльд.жб.юьтимстбоьир()
+    End Function
+
+End Class
